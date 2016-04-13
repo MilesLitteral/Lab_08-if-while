d90ab6212aa0ace55e2af5e4d4fb3519a7a8b8d1
MilesLitteral <Mandaloe2@gmail.com>
2016-04-13 11:13:10 -0400

    Added first JS assignment

---
+++ dice.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	var A = prompt("Give me your first roll");
+	var B = prompt("Give me your second roll");
+	var x = (parseInt(A) + parseInt(B));
+	alert("The sum of your rolls is: " + x);
+})
